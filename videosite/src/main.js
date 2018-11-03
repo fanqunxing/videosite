f301a9bdf23ff7dc74ed4e5ea403fb9c5e711117
fwx426328 <1053800926@qq.com>
2018-11-03 22:07:08 +0800

    调整html

--- videosite/src/main.js
+++ videosite/src/main.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 import App from './App';
 import router from './router';
-import Vuex from 'vuex';
 import store from './store/store';
-import axios from 'axios';
 
 console.log(axios);
 Vue.use(Vuex);
